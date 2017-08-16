e13764873b5eaed6d82dbdaab7c4cf7f7dbc9538
ddemingjr22 <ddemingjr22@yahoo.com>
2017-08-15 20:50:35 -0400

    Created more variable types

--- VariablesOperatorsExpressions/Program.cs
+++ VariablesOperatorsExpressions/Program.cs
@@ -10,6 +10,51 @@ namespace VariablesOperatorsExpressions
     {
         static void Main(string[] args)
         {
+            string name = "Dana";
+            //Console.WriteLine(name);
+
+            name = "Dave";
+            //Console.WriteLine(name);
+
+            string birthMonth = "September";
+            //Console.WriteLine(birthMonth);
+            //integers
+            int birthMonthNumber = 28;
+            int age = 65;
+            int population = 100000;
+
+            //floating type variables
+            float number = 2.164403304897294383961032f;
+            double someNumber = 2.164403304897294383961032d;
+            decimal partialNumber = 2.164403304897294383961032m;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(someNumber);
+            //Console.WriteLine(partialNumber);
+
+            //Boolean
+            bool isPresent = true;
+            bool seatTaken = false;
+
+            //Character
+            char lastLetter = 'd';
+
+            int jessicaAge = 23;
+            int samAge = 47;
+            Console.WriteLine(jessicaAge < samAge);
+
+            //Mike and his three friends decided to divide the bill evenly. If each person paid $13 then what was the total bill?
+            //Lisa's recipe called for 7 cups of sugar.  She put in 2 cups.  How many more cups does she need
+
+           
+
+
+
+
+
+
+
+
         }
     }
 }
